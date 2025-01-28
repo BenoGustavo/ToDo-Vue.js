3d1604a1b59c68e7bf2604a3d7a4a19679dc1525
Gustavo <gustavoleandrogorges@gmail.com>
2025-01-28 13:21:42 -0300

    added animations when changing between pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <main class="container">
-    <router-view></router-view>
+    <router-view v-slot="{ Component, route }">
+      <transition enter-active-class="animate__animated animate__zoomIn"
+        leave-active-class="animate__animated animate__zoomOut" mode="out-in">
+        <div :key="route.name">
+          <component :is="Component" />
+        </div>
+      </transition>
+    </router-view>
   </main>
 </template>
 
@@ -20,11 +27,17 @@ export default {
   }
 }
 
+.animate__animated {
+  --animate-duration: 0.5s;
+  animation-timing-function: ease-in-out;
+}
+
 body {
   background-color: var(--dark-900-color);
   font-family: var(--font-family);
   font-weight: normal;
   font-size: var(--medium-font-size);
   color: var(--secondary-200-color);
+  /* Prevent scrollbar from appearing */
 }
 </style>
